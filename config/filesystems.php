459c2adb57cd50f9c8016054044daa994a80d088
Hadi Fiqih <pratamahadi25@gmail.com>
2023-08-10 17:51:57 +0700

    fix public_path

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,6 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('ref-desain') => storage_path('app/public/ref-desain'),
     ],
 
 ];
